b5a1e2cfdd7c1922a7637318e8484b347b669fb9
Luis Armando Santamaria Garcia <156926925+Luis17xp@users.noreply.github.com>
2024-02-07 22:33:57 -0600

    Add files via upload

---
+++ ejercicio/js/interes.js
@@ -0,0 +1,23 @@
+function validar_numeros(e){
+    var teclado = (document.all)? e.keyCode : e.which;
+    if(teclado == 8) return true:
+    var patron = /[0-9\d .]/;
+    var codigo = String.fromCharCode(teclado);
+    return patron.test(codigo);
+}
+function interes(){
+    var valor = document.formulario.cantidad.value;
+    console.log(valor)
+    var resul = parseInt(valor);
+    var interes=resul *0.02;
+    var total = interes + resul;
+
+    docuemnt.formulario.sueldoI.value ="$"+total;
+}
+
+function borrar_datos(){
+    document.formulario.cantidad.value= "";
+    document.formulario.sueldoI.value= "";
+
+
+}
